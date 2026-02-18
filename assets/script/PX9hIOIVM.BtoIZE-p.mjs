41524c5ff721a954a0733e272178782f29b793c0
mads <dev@madzxla.xyz>
2026-02-18 18:29:34 +0200

    Refactor code structure for improved readability and maintainability

---
+++ assets/script/PX9hIOIVM.BtoIZE-p.mjs
@@ -0,0 +1,1 @@
+import{n as e,r as t,t as n}from"./shared-lib.DCLza4r8.mjs";t();export{e as __FramerMetadata__,n as default};
---
+++ assets/script/zFmNazs4Gzr_OU7afulWtg2J56tetQwhGE9PyR69gZo.BnahXF4v.mjs
@@ -0,0 +1,1 @@
+import{d as e,f as t,m as n,p as r}from"./shared-lib.DCLza4r8.mjs";r();export{e as __FramerMetadata__,t as default,n as queryParamNames};
